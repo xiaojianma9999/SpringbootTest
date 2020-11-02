bcec18e8f92af099cd0379737a897632a645c5c8
xiaojianma9999 <630913782@qq.com>
2020-11-02 22:27:13 +0800

    feat:新增订单配置类注释

--- src/main/java/com/xiaojianma/config/order/OrderConfig.java
+++ src/main/java/com/xiaojianma/config/order/OrderConfig.java
@@ -3,6 +3,11 @@ package com.xiaojianma.config.order;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * @Description: 订单配置类
+ * @Author: xiaojianma
+ * @Date: 2020/11/2 21:51
+ */
 @ConfigurationProperties(prefix = "spring.datasource.order")
 @Data
 public class OrderConfig {
